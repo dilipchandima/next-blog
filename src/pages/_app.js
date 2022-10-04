4a30e65588f25cf9bf7c87a67908faa041a6e7e7
dilipchandima <dilipchandima@gmail.com>
2022-10-04 10:46:12 +0530

    add tailwind

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="bg-gray-900">
+      <div className="container mx-auto">
+        <Component {...pageProps} />;
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
